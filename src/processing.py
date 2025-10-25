6ec14d54b0be701c981150983a75cc2fa69f8ea0
S.Nesterovich <stas.nester18@mail.ru>
2025-10-25 10:27:40 +0300

    add processing and.py main.py

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,13 @@
-def filter_by_state(list_slavori: list[dict], state: str = 'EXECUTED') -> list:
-    """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
-state соответствует указанному значению."""
+def filter_by_state(list_dictionaries: list[dict], state: str = 'EXECUTED') -> list[dict]:
+    """Функция возвращает новый список словарей."""
     new_list = []
-    for k in list_slavori:
+    for k in list_dictionaries:
         if k['state'] == state:
             new_list.append(k)
     return new_list
 
 
-def sort_by_date(list_slavori: list[dict], reverse: bool = True) -> list[dict]:
+def sort_by_date(list_dictionaries: list[dict], reverse: bool = True) -> list[dict]:
     """Функция возвращает новый список, отсортированный по дате."""
-    sorted_data = sorted(list_slavori, key=lambda x: x['date'], reverse=reverse)
+    sorted_data = sorted(list_dictionaries, key=lambda x: x['date'], reverse=reverse)
     return sorted_data
