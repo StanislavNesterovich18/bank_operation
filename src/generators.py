823fc9db78591f2e98abea89c777e088dc0e628e
S.Nesterovich <stas.nester18@mail.ru>
2025-11-08 12:07:56 +0300

    add generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,18 +1,29 @@
-def filter_by_currency(transactions, currency):
+from typing import Iterator, Generator
+
+
+def filter_by_currency(transactions: list, currency: str) -> Iterator:
+    """
+    Функция возвращать итератор,
+     который поочередно выдает транзакции,
+      где валюта операции соответствует заданной
+      """
     for transact in transactions:
         if transact["operationAmount"]["currency"]["code"] == currency:
             yield transact
 
 
-def transaction_descriptions(transactions):
+def transaction_descriptions(transactions: list) -> Iterator:
+    """
+    Принимает список словарей с транзакциями и возвращает
+     описание каждой операции по очереди
+     """
     for transact in transactions:
         yield transact["description"]
 
 
-def card_number_generator(start, end):
+def card_number_generator(start: int, end: int) -> Generator:
+    """Генератор может сгенерировать номера карт в заданном диапазоне"""
     for x in range(start, end + 1):
         card_number = f"{x:016d}"
         card_number_str = " ".join([card_number[x: x + 4] for x in range(0, 16, 4)])
         yield card_number_str
-
-
