5f2a98ec5c8efdf33195a9a3e839d956154b6f20
S.Nesterovich <stas.nester18@mail.ru>
2025-11-25 14:30:17 +0300

    add generators.py

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,32 @@
+import logging
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('logs/masks.log', "w", "utf-8")
+file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(number_card: str) -> str:
     """Функцию маскировки номера банковской карты"""
+    logger.info("запуск функции get_mask_card_number")
     card_namber_str = number_card.replace(" ", "")
     if len(card_namber_str) == 16:
+        logger.info("замаскированный номер карты")
         return card_namber_str[:4] + " " + card_namber_str[4:6] + "** **** " + card_namber_str[-4:]
     else:
+        logger.error("ошибка: неправильный номер карты")
         return "Вы ввели неправильный номер карты"
 
 
 def get_mask_account(number_chek: str) -> str:
     """Функцию маскировки номера банковского счета"""
+    logger.info("запуск функции get_mask_account")
     chek_number_str = number_chek.replace(" ", "")
     if len(chek_number_str) == 20 and chek_number_str.isdigit():
+        logger.info("замаскированный номер счета")
         return "**" + chek_number_str[-4:]
     else:
+        logger.error("ошибка: неправильный номер счета")
         return "Вы ввели неправильный номер счета"
--- src/utils.py
+++ src/utils.py
@@ -1,28 +1,44 @@
 import json
 import os
+import logging
 from src.external_api import api_currency
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('logs/utils.log', "w", "utf-8")
+file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def financial_transaction(file_path: str = "") -> list[dict]:
     """Функция открывает json файл"""
+    logger.info("Запуск функции financial_transaction")
     if file_path == "":
         utils_dir = os.path.dirname(os.path.abspath(__file__))
         file_path = os.path.join(utils_dir, "..", "data", "operations.json")
+        logger.info("Путь до файла json")
     try:
         with open(file_path, "r", encoding="utf-8") as f:
             data = json.load(f)
+            logger.info("Открытие файла json в python")
         return data
-    except Exception:
+    except Exception as e:
+        logger.error(f"ошибка {e}")
         return []
 
 
 def amount_transaction(transactions: list[dict]) -> list[dict]:
-    """Функция возвращает сумму транзакции в рублях"""
+    """Запуск функции amount_transaction"""
+    logger.info("Запуск функции financial_transaction")
     if transactions.get('operationAmount', {}).get('currency', {}).get('code') == 'RUB':  # type: ignore
+        logger.info("Возвращает сумму транзакции")
         return transactions['operationAmount']['amount']  # type: ignore
     elif transactions.get('operationAmount', {}).get('currency', {}).get('code', None) is None:  # type: ignore
+        logger.error("Валюта не найдена")
         return "Валюта не найдена"  # type: ignore
     else:
         convert_currency = api_currency(transactions['operationAmount']['currency']['code'],  # type: ignore
                                         transactions['operationAmount']['amount'])  # type: ignore
+        logger.info("Возвращает сумму транзакции конвертируя в рубли")
         return convert_currency
